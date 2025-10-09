f2d983ed9144bbfce447b70a9ddb837cce548b3f
paramasivam2709 <paramasivamb2000@gmail.com>
2025-10-09 21:25:16 +0530

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,20 +1,32 @@
+// ...existing code...
 function initialize() {
-	var myOptions = {
-		zoom: 16,
-		center: new google.maps.LatLng(51.489500, -0.096777), //change the coordinates
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		scrollwheel: false,
-		mapTypeControl: false,
-		zoomControl: false,
-		streetViewControl: false,
-		}
-	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	var marker = new google.maps.Marker({
-		map: map,
-		position: new google.maps.LatLng(51.489500, -0.096777) //change the coordinates
-	});
-	google.maps.event.addListener(marker, "click", function() {
-		infowindow.open(map, marker);
-	});
+    // Stackly Salem office coordinates
+    var stacklySalem = new google.maps.LatLng(11.6643, 78.1460);
+
+    var myOptions = {
+        zoom: 16,
+        center: stacklySalem,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false,
+    }
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+    // Info window for Stackly Salem office
+    var infoContent = '<div style="font-weight:700">Stackly Salem Office</div><div>Salem, Tamil Nadu, India</div>';
+    var infowindow = new google.maps.InfoWindow({ content: infoContent });
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: stacklySalem,
+        title: 'Stackly Salem Office'
+    });
+
+    google.maps.event.addListener(marker, "click", function() {
+        infowindow.open(map, marker);
+    });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+// ...existing code...
